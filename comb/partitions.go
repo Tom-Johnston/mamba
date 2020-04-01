6b434bba5b47a08ccd9c758968c8147a47505195
Tom Johnston <tom.johnston5861@gmail.com>
2020-04-01 12:39:47 +0100

    Add an iterator which iterates over set partitions

---
+++ comb/partitions.go
@@ -0,0 +1,99 @@
+package comb
+
+func totalLength(a [][]int) int {
+	n := 0
+	for _, s := range a {
+		n += len(s)
+	}
+	return n
+}
+
+//The restricted growth string of a partition is the sequence of numbers r[0], ..., r[n-1] where  r[i] is the index of the set that i is in when the sets are ordered in increasing size of their smallest element.
+//sets must be ordered in increasing size of their smallest element.
+func restrictedGrowthString(sets [][]int) []int {
+	rgs := make([]int, totalLength(sets))
+	curr := 0
+	for _, set := range sets {
+		for _, j := range set {
+			rgs[j] = curr
+		}
+		curr++
+	}
+	return rgs
+}
+
+func partitionFromRestrictedGrowthString(rgs []int) [][]int {
+	n := len(rgs)
+	max := 0
+	sizes := make([]int, n)
+	for _, v := range rgs {
+		sizes[v]++
+		if v > max {
+			max = v
+		}
+	}
+
+	p := make([][]int, max+1)
+	for i := range p {
+		p[i] = make([]int, 0, sizes[i])
+	}
+	for i, v := range rgs {
+		p[v] = append(p[v], i)
+	}
+	return p
+}
+
+//PartitionIterator iterates over all partitions of the set {0, ..., n-1}.
+//The partitions are generated in lexicographic order of their restricted growth strings. It is safe to modify the output of .Value().
+type PartitionIterator struct {
+	n int
+	m int
+	a []int
+	b []int
+}
+
+//TODO Handle n = 0
+//NewPartitionIterator returns a *PartitionIterator which iterates over all partitions of the set {0, ..., n-1}.
+//The partitions are generated in lexicographic order of their restricted growth sequences.
+func NewPartitionIterator(n int) *PartitionIterator {
+	if n < 1 {
+		panic("Cannot handle n < 1")
+	}
+	a := make([]int, n)
+	a[n-1] = -1
+	b := make([]int, n)
+	for i := range b {
+		b[i] = 1
+	}
+	return &PartitionIterator{n: n, m: 1, a: a, b: b}
+}
+
+//Next tries to advance pi to the next partition, returning true if there is one and false if there isn't.
+func (pi *PartitionIterator) Next() bool {
+	if pi.a[pi.n-1] == pi.m {
+		for j := pi.n - 2; j >= 1; j-- {
+			if pi.a[j] != pi.b[j] {
+				pi.a[j]++
+				pi.m = pi.b[j]
+				if pi.a[j] == pi.b[j] {
+					pi.m++
+				}
+				for k := j + 1; k < pi.n-1; k++ {
+					pi.a[k] = 0
+					pi.b[k] = pi.m
+				}
+				pi.a[pi.n-1] = 0
+				return true
+			}
+		}
+		return false
+	}
+	pi.a[pi.n-1]++
+	return true
+}
+
+//Value returns the current partition.
+//It is safe to modify the output.
+func (pi PartitionIterator) Value() [][]int {
+	return partitionFromRestrictedGrowthString(pi.a)
+}
---
+++ comb/partitions_test.go
@@ -0,0 +1,51 @@
+package comb
+
+import (
+	"testing"
+
+	"github.com/Tom-Johnston/gigraph/ints"
+)
+
+func TestPartitionIterator(t *testing.T) {
+	//Truth data
+	correctOutput := [][][]int{
+		[][]int{[]int{0, 1, 2, 3}},
+		[][]int{[]int{0, 1, 2}, []int{3}},
+		[][]int{[]int{0, 1, 3}, []int{2}},
+		[][]int{[]int{0, 1}, []int{2, 3}},
+		[][]int{[]int{0, 1}, []int{2}, []int{3}},
+		[][]int{[]int{0, 2, 3}, []int{1}},
+		[][]int{[]int{0, 2}, []int{1, 3}},
+		[][]int{[]int{0, 2}, []int{1}, []int{3}},
+		[][]int{[]int{0, 3}, []int{1, 2}},
+		[][]int{[]int{0}, []int{1, 2, 3}},
+		[][]int{[]int{0}, []int{1, 2}, []int{3}},
+		[][]int{[]int{0, 3}, []int{1}, []int{2}},
+		[][]int{[]int{0}, []int{1, 3}, []int{2}},
+		[][]int{[]int{0}, []int{1}, []int{2, 3}},
+		[][]int{[]int{0}, []int{1}, []int{2}, []int{3}},
+	}
+
+	pi := NewPartitionIterator(4)
+	index := 0
+	for pi.Next() {
+		if index >= len(correctOutput) {
+			t.Log("Found too many partitions.")
+			t.FailNow()
+		}
+		found := pi.Value()
+		truth := correctOutput[index]
+		if len(found) != len(truth) {
+			t.Fail()
+		}
+		for i := range found {
+			if !ints.Equal(found[i], truth[i]) {
+				t.Error(found[i], truth[i])
+			}
+		}
+		index++
+	}
+	if index != len(correctOutput) {
+		t.Error("Found too few partitions.")
+	}
+}
