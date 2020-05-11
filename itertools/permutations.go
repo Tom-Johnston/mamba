20b147a2c5abf7345de79614bfdcfe06ec2a39b4
Tom-Johnston <tom.johnston5861@gmail.com>
2020-05-11 14:39:23 +0100

    Add iterators for permutations in lexicographic order and permutations of a multiset

--- itertools/permutations.go
+++ itertools/permutations.go
@@ -1,5 +1,9 @@
 package itertools
 
+import (
+	"github.com/Tom-Johnston/mamba/ints"
+)
+
 //PermutationIterator is a struct containing the state of the iterator.
 //It iterates over permutations according to Heap's algorithm.
 type PermutationIterator struct {
@@ -51,4 +55,106 @@ func (p *PermutationIterator) Next() bool {
 		p.i++
 	}
 	return false
+}
+
+//LexicographicPermutationIterator is a struct contraining the state of an iterator which iterates over all permutations of {0, 1, ..., n-1} in lexicographic order.
+type LexicographicPermutationIterator struct {
+	n     int
+	a     []int
+	first bool
+}
+
+//LexicographicPermutations returns a new iterator which iterates over all permutations of {0, ..., n-1} in lexicographic order.
+//It is not safe to modify the output of the iterator.
+func LexicographicPermutations(n int) *LexicographicPermutationIterator {
+	a := make([]int, n)
+	for i := range a {
+		a[i] = i
+	}
+	return &LexicographicPermutationIterator{n: n, a: a, first: true}
+}
+
+//Value returns the current permutation.
+//You must not modify the output of this function.
+func (iter *LexicographicPermutationIterator) Value() []int {
+	return iter.a
+}
+
+//Next moves the iterator to the next permutation, returning true if there is one and false if the previous permutation is the last one.
+func (iter *LexicographicPermutationIterator) Next() bool {
+	n := iter.n
+	if n > 0 && iter.first {
+		iter.first = false
+		return true
+	}
+
+	if n > 1 && iter.a[n-2] < iter.a[n-1] {
+		iter.a[n-2], iter.a[n-1] = iter.a[n-1], iter.a[n-2]
+		return true
+	}
+
+	if n > 2 && iter.a[n-3] < iter.a[n-2] {
+		if iter.a[n-3] < iter.a[n-1] {
+			iter.a[n-3], iter.a[n-2], iter.a[n-1] = iter.a[n-1], iter.a[n-3], iter.a[n-2]
+		} else {
+			iter.a[n-3], iter.a[n-2], iter.a[n-1] = iter.a[n-2], iter.a[n-1], iter.a[n-3]
+		}
+		return true
+	}
+
+	for j := iter.n - 4; j >= 0; j-- {
+		if iter.a[j] >= iter.a[j+1] {
+			continue
+		}
+		if iter.a[j] < iter.a[n-1] {
+			iter.a[j], iter.a[j+1], iter.a[n-1] = iter.a[n-1], iter.a[j], iter.a[j+1]
+		} else {
+			for l := n - 2; l > 0; l-- {
+				if iter.a[j] >= iter.a[l] {
+					continue
+				}
+				iter.a[j], iter.a[l] = iter.a[l], iter.a[j]
+				iter.a[n-1], iter.a[j+1] = iter.a[j+1], iter.a[n-1]
+				break
+			}
+		}
+		k := j + 2
+		l := n - 2
+		for k < l {
+			iter.a[k], iter.a[l] = iter.a[l], iter.a[k]
+			k++
+			l--
+		}
+		return true
+	}
+	return false
+}
+
+//MultisetPermutationIterator is a struct containing the state of an iterator which iterates over all permutations of some multiset from {0, 1, ..., n-1} in lexicographic order.
+type MultisetPermutationIterator struct {
+	lexIter *LexicographicPermutationIterator
+}
+
+//MultisetPermutations returns an iterator which iterates over all permutations of some multiset from {0, 1, ..., n-1} in lexicographic order. The number i appears freq[i] times in the multiset.
+//It is not safe to modify the output of the iterator.
+func MultisetPermutations(freq []int) *MultisetPermutationIterator {
+	n := ints.Sum(freq)
+	a := make([]int, 0, n)
+	for i := range freq {
+		for j := 0; j < freq[i]; j++ {
+			a = append(a, i)
+		}
+	}
+	return &MultisetPermutationIterator{lexIter: &LexicographicPermutationIterator{n: n, a: a, first: true}}
+}
+
+//Value returns the current permutation.
+//You must not modify the output of this function.
+func (iter *MultisetPermutationIterator) Value() []int {
+	return iter.lexIter.Value()
+}
+
+//Next moves the iterator to the next permutation, returning true if there is one and false if the previous permutation is the last one.
+func (iter *MultisetPermutationIterator) Next() bool {
+	return iter.lexIter.Next()
 }
--- itertools/permutations_test.go
+++ itertools/permutations_test.go
@@ -1,6 +1,8 @@
 package itertools
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPermutations(t *testing.T) {
 	iter := Permutations(4)
@@ -17,6 +19,53 @@ func TestPermutations(t *testing.T) {
 		}
 	}
 	if i != 23 {
+		t.Fail()
+	}
+}
+
+func TestLexicographicPermutations(t *testing.T) {
+	iter := LexicographicPermutations(4)
+	correstResult := [][]int{{0, 1, 2, 3}, {0, 1, 3, 2}, {0, 2, 1, 3}, {0, 2, 3, 1}, {0, 3, 1, 2}, {0, 3, 2, 1}, {1, 0, 2, 3}, {1, 0, 3, 2}, {1, 2, 0, 3}, {1, 2, 3, 0}, {1, 3, 0, 2}, {1, 3, 2, 0}, {2, 0, 1, 3}, {2, 0, 3, 1}, {2, 1, 0, 3}, {2, 1, 3, 0}, {2, 3, 0, 1}, {2, 3, 1, 0}, {3, 0, 1, 2}, {3, 0, 2, 1}, {3, 1, 0, 2}, {3, 1, 2, 0}, {3, 2, 0, 1}, {3, 2, 1, 0}}
+	i := -1
+	for iter.Next() {
+		i++
+		v := iter.Value()
+		w := correstResult[i]
+		for j := range w {
+			if w[j] != v[j] {
+				t.Fail()
+			}
+		}
+	}
+	if i != 23 {
+		t.Fail()
+	}
+}
+
+func TestMultisetPermutations(t *testing.T) {
+	iter := MultisetPermutations([]int{1, 2, 1})
+	correstResult := [][]int{{0, 1, 1, 2}, {0, 1, 2, 1}, {0, 2, 1, 1}, {1, 0, 1, 2}, {1, 0, 2, 1}, {1, 1, 0, 2}, {1, 1, 2, 0}, {1, 2, 0, 1}, {1, 2, 1, 0}, {2, 0, 1, 1}, {2, 1, 0, 1}, {2, 1, 1, 0}}
+	i := -1
+	for iter.Next() {
+		i++
+		v := iter.Value()
+		w := correstResult[i]
+		for j := range w {
+			if w[j] != v[j] {
+				t.Fail()
+			}
+		}
+	}
+	if i != 11 {
+		t.Fail()
+	}
+
+	iter = MultisetPermutations([]int{2, 2, 2})
+	i = -1
+	for iter.Next() {
+		i++
+	}
+	if i != 89 {
 		t.Fail()
 	}
 }
