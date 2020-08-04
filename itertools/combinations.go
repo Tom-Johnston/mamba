2e9875b744e8fca4b4e168f82a58530273f929c1
Tom-Johnston <tom.johnston5861@gmail.com>
2020-08-04 22:17:25 +0100

    Add an iterator for combinations in colex order
    This is the same order as the ranking and unranking functions in comb.

--- itertools/combinations.go
+++ itertools/combinations.go
@@ -43,3 +43,69 @@ func Combinations(n, k int) *CombinationIterator {
 	}
 	return &CombinationIterator{n: n, k: k, data: data}
 }
+
+//CombinationColexIterator iterates over all ways of chooding k elements from 0, ..., n-1 in colexicographic order.
+//It can be initialised by calling CombinationsColex.
+type CombinationColexIterator struct {
+	n    int
+	k    int
+	j    int //Position to try to increase
+	data []int
+}
+
+//CombinationsColex returns a new CombinationColexIterator which iterates over all subsets of k distinct elements from 0, ..., n-1 in colexicographic order.
+func CombinationsColex(n, k int) *CombinationColexIterator {
+	data := make([]int, k)
+	for i := 0; i < k; i++ {
+		data[i] = i
+	}
+
+	if k > 0 {
+		data[k-1]--
+	}
+	return &CombinationColexIterator{n: n, k: k, j: k, data: data}
+}
+
+//Value returns the current subset. The output must not be modified.
+func (b CombinationColexIterator) Value() []int {
+	return b.data
+}
+
+//Next attempts to advance the iterator to the next subset, returning true if there is one and false if not.
+func (b *CombinationColexIterator) Next() bool {
+	if b.k <= 0 {
+		b.k--
+		return b.k == -1
+	}
+
+	if b.j >= b.k-1 {
+		if b.data[b.k-1] == b.n-1 {
+			return false
+		}
+		b.data[b.k-1]++
+		b.j--
+		return true
+	}
+
+	if b.j != -1 {
+		b.data[b.j]++
+		b.j--
+		return true
+	}
+
+	for j := 0; j < b.k-1; j++ {
+		if b.data[j] < b.data[j+1]-1 {
+			b.data[j]++
+			b.j = j - 1
+			return true
+		}
+		b.data[j] = j
+	}
+
+	if b.data[b.k-1] == b.n-1 {
+		return false
+	}
+	b.data[b.k-1]++
+	b.j = b.k - 2
+	return true
+}
--- itertools/combinations_test.go
+++ itertools/combinations_test.go
@@ -33,3 +33,39 @@ func TestCombinations(t *testing.T) {
 		t.Error("Found too few combinations.")
 	}
 }
+
+func TestCombinationsColex(t *testing.T) {
+	//Truth data
+	inputParameters := [][]int{{4, 0}, {5, 1}}
+	correctOutputs := [][][]int{
+		{{}},
+		{{0}, {1}, {2}, {3}, {4}},
+	}
+	for i := range inputParameters {
+		inputs := inputParameters[i]
+		correctOutput := correctOutputs[i]
+		iter := CombinationsColex(inputs[0], inputs[1])
+		index := 0
+		for iter.Next() {
+			if index >= len(correctOutput) {
+				t.Log(index)
+				t.Log("Found too many combinations.")
+				t.FailNow()
+			}
+			found := iter.Value()
+			truth := correctOutput[index]
+			if len(found) != len(truth) {
+				t.Fail()
+			}
+			for i := range found {
+				if found[i] != truth[i] {
+					t.Fail()
+				}
+			}
+			index++
+		}
+		if index != len(correctOutput) {
+			t.Error("Found too few combinations.")
+		}
+	}
+}
