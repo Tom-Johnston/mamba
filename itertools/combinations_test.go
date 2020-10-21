d43201115f5001f65ad52cd5de81ebcc8181c111
Tom Johnston <tom.johnston5861@gmail.com>
2020-10-21 22:57:38 +0100

    Improve tests for combinations

--- itertools/combinations_test.go
+++ itertools/combinations_test.go
@@ -6,40 +6,50 @@ import (
 
 func TestCombinations(t *testing.T) {
 	//Truth data
-	correctOutput := [][]int{
-		{},
+	inputParameters := [][]int{{0, 0}, {4, 0}, {5, 1}, {5, 2}}
+	correctOutputs := [][][]int{
+		{{}},
+		{{}},
+		{{0}, {1}, {2}, {3}, {4}},
+		{{0, 1}, {0, 2}, {0, 3}, {0, 4}, {1, 2}, {1, 3}, {1, 4}, {2, 3}, {2, 4}, {3, 4}},
 	}
-
-	iter := Combinations(4, 0)
-	index := 0
-	for iter.Next() {
-		if index >= len(correctOutput) {
-			t.Log("Found too many combinations.")
-			t.FailNow()
-		}
-		found := iter.Value()
-		truth := correctOutput[index]
-		if len(found) != len(truth) {
-			t.Fail()
-		}
-		for i := range found {
-			if found[i] != truth[i] {
+	for i := range inputParameters {
+		inputs := inputParameters[i]
+		correctOutput := correctOutputs[i]
+		iter := Combinations(inputs[0], inputs[1])
+		index := 0
+		for iter.Next() {
+			if index >= len(correctOutput) {
+				t.Log(index)
+				t.Log("Found too many combinations.")
+				t.FailNow()
+			}
+			found := iter.Value()
+			truth := correctOutput[index]
+			if len(found) != len(truth) {
 				t.Fail()
 			}
+			for i := range found {
+				if found[i] != truth[i] {
+					t.Fail()
+				}
+			}
+			index++
 		}
-		index++
-	}
-	if index != len(correctOutput) {
-		t.Error("Found too few combinations.")
+		if index != len(correctOutput) {
+			t.Error("Found too few combinations.")
+		}
 	}
 }
 
 func TestCombinationsColex(t *testing.T) {
 	//Truth data
-	inputParameters := [][]int{{4, 0}, {5, 1}}
+	inputParameters := [][]int{{0, 0}, {4, 0}, {5, 1}, {5, 2}}
 	correctOutputs := [][][]int{
 		{{}},
+		{{}},
 		{{0}, {1}, {2}, {3}, {4}},
+		{{0, 1}, {0, 2}, {1, 2}, {0, 3}, {1, 3}, {2, 3}, {0, 4}, {1, 4}, {2, 4}, {3, 4}},
 	}
 	for i := range inputParameters {
 		inputs := inputParameters[i]
